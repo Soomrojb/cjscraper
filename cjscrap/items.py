54d298b36185d10b7b3bc438a83833f17ebf72d0
Janib Soomro <Soomrojb@gmail.com>
2017-03-06 11:56:01 +0000

    Defining data fields

--- cjscrap/items.py
+++ cjscrap/items.py
@@ -1,14 +1,10 @@
 # -*- coding: utf-8 -*-
 
-# Define here the models for your scraped items
-#
-# See documentation in:
-# http://doc.scrapy.org/en/latest/topics/items.html
-
 import scrapy
 
-
 class CjscrapItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+	posturl = scrapy.Field()
+	time = scrapy.Field()
+	posttitle = scrapy.Field()
+	parse_posts = scrapy.Field()
+
