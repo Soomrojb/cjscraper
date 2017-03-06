42b2632103536da0b113b8b05699d67fd07349c4
Janib Soomro <Soomrojb@gmail.com>
2017-03-06 11:58:09 +0000

    Updating main spider after setting Random Useragent & MySQLdb Pipeline

--- cjscrap/spiders/spider.py
+++ cjscrap/spiders/spider.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 from scrapy import Request, Selector
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors.lxmlhtml import LxmlLinkExtractor
+from cjscrap.item import CjscrapItem
 
 class CJScrapper(CrawlSpider):
 	name = "cjscrap"
@@ -22,32 +23,16 @@ class CJScrapper(CrawlSpider):
 	)
 	
 	def parse_list(self, response):
-		#Html = Selector(response)
-		#Title = Html.xpath("//nav/div/h2")
-		#CompGig = Html.xpath("//div/ul/li/a[contains(@href,'/search/cpg')]")
-		#print response.url + "search/cpg"
-		#yield {
-		#	'test' : CompGig.extract(),
-		#	'title' : Title.extract()[0],
-		#	'url' : response.url,
-		#	'parse_list' : (response.url + "/search/cpg"),
-		#}
-		#print response
-		#Html = BeautifulSoup(response.body, 'html.parser')
-		#NextURl = Html.find('a').get('href')
-		#print NextURl
-		#yield Request(response.urljoin(CompGig.extract()), callback=self.parse)
 		yield Request((response.url + "search/cpg"), callback=self.parse_posts)
 
 	def parse_posts(self, response):
-		#print "new url = " + response.url
+		items = CjscrapItem()
 		Html = Selector(response)
 		RowTitle = Html.xpath("//ul[contains(@class,'row')]/li//p")
 		for Post in RowTitle:
-			yield {
-				'posturl' : response.urljoin(Post.xpath("a/@href").extract()[0]),
-				'time' : Post.xpath("time/@datetime").extract(),
-				'posttitle' : Post.xpath("a/text()").extract(),
-				'parse_posts' : response.url,
-			}
+			items['posturl'] = response.urljoin(Post.xpath("a/@href").extract()[0])
+			items['time'] = Post.xpath("time/@datetime").extract()
+			items['posttitle'] = Post.xpath("a/text()").extract()
+			items['parse_posts'] = response.url
+			yield items
 
