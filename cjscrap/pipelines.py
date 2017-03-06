da762ab38a9ec4842f92e3a8323cd19a363c1a70
Janib Soomro <Soomrojb@gmail.com>
2017-03-06 11:56:38 +0000

    Routing data directly to MySQLdb, defining custom Pipeline

--- cjscrap/pipelines.py
+++ cjscrap/pipelines.py
@@ -1,11 +1,52 @@
 # -*- coding: utf-8 -*-
 
-# Define your item pipelines here
+# Database Structure
+#	craigslistdb:
+#		cjposts:
+#			- serial (auto)
+#			- posturl (text)
+#			- time (text)
+#			- posttitle (text)
+#			- parse_posts (text)
 #
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+import MySQLdb
 
-class CjscrapPipeline(object):
+class CraigsListPipeline(object):
     def process_item(self, item, spider):
-        return item
+		conn = None
+		curr = None
+		query = "INSERT INTO cjposts (posturl, time, posttitle, parse_posts) VALUES (%s, %s, %s, %s)"
+		selquery = "SELECT * FROM cjposts WHERE `posturl` = %s AND `posttitle` = %s"
+		
+	def __init__(self):
+		self.setupDBCon()
+	
+	def __del__(self):
+		self.conn.close()
+	
+	def setupDBCon(self):
+		self.conn = MySQLdb.connect(host='localhost', user='admin', passwd='admin', db='craigslistdb')
+		self.curr = self.conn.cursor()
+
+	def open_spider(self, spider):
+		print
+		print "----------------------------------------------"
+		print "	Starting Craigslist Scrapper"
+		print "----------------------------------------------"
+		print
+
+	def close_spider(self, spider):
+		print
+		print "----------------------------------------------"
+		print "	Ending Craigslist Scrapper"
+		print "----------------------------------------------"
+		print
+
+	def process_item(self, item, spider):
+		self.curr.execute(self.selquery, (item['posturl'], item['posttitle']))
+		if self.curr.fetchone() is None:
+			self.curr.execute(self.query, (item['posturl'], item['time'], item['posttitle'], item['parse_posts']))
+			self.conn.commit()
+		return item
+	
